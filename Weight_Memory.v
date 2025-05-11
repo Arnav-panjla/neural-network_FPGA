f2daad47844c73f4d630d29b52632cb4ebf37f2d
Arnav-panjla <arnavpanjla@gmail.com>
2025-05-12 02:22:56 +0530

    midified some files

--- Weight_Memory.v
+++ Weight_Memory.v
@@ -1,38 +1,41 @@
 `include "include.v"
 
-module Weight_Memory #(parameter numWeight = 3, neuronNo = 5, weightNo = 3, dataWidth = 16, addressWidth = 5, weightFile = "weights_1.mif")
-    (
-        input clk,
-        input wen,
-        input ren,
-        input [addressWidth-1:0] wadd,
-        input [addressWidth-1:0] radd,
-        input [dataWidth-1:0] win,
-        output reg [dataWidth-1:0] wout
-    );
-
+module Weight_Memory #(parameter numWeight = 3, neuronNo=5,layerNo=1,addressWidth=10,dataWidth=16,weightFile="weights.mif") 
+    ( 
+    input clk,
+    input wen,
+    input ren,
+    input [addressWidth-1:0] wadd,
+    input [addressWidth-1:0] radd,
+    input [dataWidth-1:0] win,
+    output reg [dataWidth-1:0] wout);
+    
     reg [dataWidth-1:0] mem [numWeight-1:0];
 
     `ifdef pretrained
-        initial begin
-            $readmemb(weightFile, mem); // b for binary, h for hex
-        end
-    `else
-        always @(posedge clk) 
-        begin
-            if (wen) begin
-                mem[wadd] <= win;
-            end            
-        end
-    `endif 
+        initial
+		begin
+	        $readmemb(weightFile, mem);
+	    end
+	`else
+		always @(posedge clk)
+		begin
+			if (wen)
+			begin
+				mem[wadd] <= win;
+			end
+		end 
+    `endif
+    
     always @(posedge clk)
     begin
-        if (ren) begin
+        if (ren)
+        begin
             wout <= mem[radd];
         end
-        else begin
-            wout <= 0;
+        else 
+        begin
+            wout <= {dataWidth{1'b0}};
         end
     end 
-
 endmodule
